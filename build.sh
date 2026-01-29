082b11dc82f885f44166b30d9aaa90bdf87006f6
Hydrolox <me@hydrolox.me>
2026-01-29 22:13:58 +0100

    Add scene data

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ mkdir -p customlevel_bin/pc-w
 
 build_ob level-level.txtdat level-level.dat
 build_ob level-zadmd.txtdat level-zadmd.dat
+build_ob level-cellgroup.txtdat level-cellgroup.dat
 build_ob level.txtdat level.dat
 cp objlist.dat customlevel_bin/pc-w/objlist.dat
 python3 build-drm.py mytestlevel.txtdrm customlevel_bin/pc-w/mytestlevel.drm
