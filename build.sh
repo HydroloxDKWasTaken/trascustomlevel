0beed2cf59827d44e4663377045a4b6e24781a32
Hydrolox <me@hydrolox.me>
2026-01-29 21:26:29 +0100

    Add ADMD

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ MKLOADOB="/mnt/d/Programming/Projects/cdc_lib/build_win/tools/resource_mkloadob/
 build_ob () {
     name=$1
     dest=$2
-    if test ${name} -nt ${dest}
+    if test ${name} -nt ${dest} || [ ! -f ${dest} ]
     then
         ${MKLOADOB} ${name} ${dest}
         echo "Rebuilt '${name}'"
@@ -13,6 +13,7 @@ build_ob () {
 mkdir -p customlevel_bin/pc-w
 
 build_ob level-level.txtdat level-level.dat
+build_ob level-zadmd.txtdat level-zadmd.dat
 build_ob level.txtdat level.dat
 cp objlist.dat customlevel_bin/pc-w/objlist.dat
 python3 build-drm.py mytestlevel.txtdrm customlevel_bin/pc-w/mytestlevel.drm
