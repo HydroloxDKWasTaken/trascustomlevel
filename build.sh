cdf81559d987fe74f625965a609e44c684072a72
Hydrolox <me@hydrolox.me>
2026-02-28 14:37:52 +0100

    Add collision mesh

--- build.sh
+++ build.sh
@@ -25,6 +25,10 @@ build_ob level-level.txtdat level-level.dat
 build_ob level-zadmd.txtdat level-zadmd.dat
 build_ob level-cellgroup.txtdat level-cellgroup.dat
 build_ob level-meshimf.txtdat level-meshimf.dat
+# TODO: this only needs to be rebuilt if level-mesh.obj changes
+python3 build-colmesh.py level-cmesh.obj level-cmesh.cmeshtxt
+build_ob level-cmesh.cmeshtxt level-cmesh.dat
+build_ob level-tg1.txtdat level-tg1.dat
 build_ob level.txtdat level.dat
 cp objlist.dat customlevel_bin/pc-w/objlist.dat
 python3 build-drm.py mytestlevel.txtdrm customlevel_bin/pc-w/mytestlevel.drm
